44209495ab3733238cc877a77966e074750e6663
Paul Work <paulusojak@gmail.com>
2025-12-16 13:42:45 +0700

    Modify config.go

--- config.go
+++ config.go
@@ -15,3 +15,26 @@ type UserLogin struct {
 	Email    string    `json:"email"`
 	Password string    `json:"password"`
 }
+
+type User struct {
+	ID	   uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	Pass string    `json:"pass"`
+}
+
+type MimixObj struct {
+	ID      uuid.UUID `json:"id"`
+	Obj   	string    `json:"obj"`
+	ObjType string    `json:"obj_type"`
+	PromoteDate time.Time `json:"promote_date"`
+	ObjVer  string    `json:"obj_ver"`
+	Lib 	string   `json:"lib"`
+	MimixStatus string    `json:"mimix_status"`
+}
+
+type MimixLib struct {
+	ID      uuid.UUID `json:"id"`
+	Lib	 string    `json:"lib"`
+}
+
+
